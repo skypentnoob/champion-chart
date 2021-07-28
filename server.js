5af09f4379ea47e09396f15778b9ee0acbf734f2
yes <67612584+PicturemyBootyUpin3d@users.noreply.github.com>
2021-07-27 20:25:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-var express = require("express")
-var
+var express = require("express") // i am copying code
+var app = express() // because i am lazy
+
+var port = process.env.PORT || 8080
+
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", function(req,res){
+       res.render("index")
+})
+
+app.listen(port, function() {
+  console.log("app running")
+})
